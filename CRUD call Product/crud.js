e17dca56ad3f925c0b21a6c12b4d7070dc1dca1d
Zyam-1 <zyammaqsood7@gmail.com>
2022-09-30 18:53:37 +0500

    sd

---
+++ CRUD call Product/crud.js
@@ -0,0 +1,86 @@
+$(function(){
+    loadRecipes()
+    $("#AddPro").click(AddProduct);
+    $("#main-cont").on("click", "#DelBtn", handleDel);
+    $("#main-cont").on("click", "#EditBtn", handleEdit);
+
+
+})
+
+function handleEdit(){
+    $("#EditModal").modal("show");
+    var EditBtn = $("this");
+    var ParentDiv = EditBtn.closest(".border");
+    var id = ParentDiv.attr("data-id");
+    $.ajax ({
+        url: "https://usman-recipes.herokuapp.com/api/products/" + id,
+        method: "GET",
+        success: function(response){
+
+            console.log(response)
+            // $("#UpdateName").val = response.name;
+            // $("#UpdatePrice").val = response.price;
+            // $("#UpdateColor").val = response.color;
+            // $("#UpdateDep").val = response.department;
+            // $("#UpdateDescr").val = response.description;
+             loadRecipes()
+            
+        }
+    })
+
+}
+
+function handleDel(){
+    var DeleteBtn = $(this);
+    var ParentDiv = DeleteBtn.closest(".border");
+    var id = ParentDiv.attr("data-id");
+    $.ajax ({
+        url : "https://usman-recipes.herokuapp.com/api/products/" + id,
+        method : "DELETE",
+        success: function(response){
+            loadRecipes()
+        }
+    })
+}
+
+function AddProduct(){
+      var NewProName = $("#NewName").val();
+      var NewProPrice = $("#NewPrice").val();
+      var NewProColor = $("#NewColor").val();
+      var NewProDep = $("#NewDep").val();
+      var NewDescriptionPro = $("#NewDescription").val();
+
+      $.ajax ({
+        url : "https://usman-recipes.herokuapp.com/api/products",
+        method: "POST",
+        data: {name: NewProName, price: NewProPrice, color: NewProColor, department: NewProDep, description: NewDescriptionPro},
+        success: function(response){
+            loadRecipes();
+            $("#AddProductmodel").modal("hide");
+
+        }
+      })
+      
+      
+
+
+
+    
+}
+
+function loadRecipes(){
+    var MainCont = $("#main-cont");
+    
+    $.ajax({
+        url : "https://usman-recipes.herokuapp.com/api/products",
+        method: "GET",
+        success: function(response){
+            MainCont.empty();
+            for ( var i = 0; i < response.length; i++){
+                var Product = response[i];
+                MainCont.append(`<div class = "border p-1" data-id = "${Product._id}"><button class = "btn btn-secondary float-right" id = "EditBtn">Edit</button><button class = "btn btn-primary float-right" id = "DelBtn">Delete</button><h3>Name: ${Product.name}</h3><h4>Price: $${Product.price}</h4><h5>Product Color: ${Product.color}</h5><h6>Department: ${Product.department}</h6><p>Description: ${Product.description}</p<</div>`)
+            }
+
+        }
+    }) 
+}
