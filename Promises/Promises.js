8f19ea9bb05d1bbf4059ed762cca3bfd92c060e5
Zyam-1 <zyammaqsood7@gmail.com>
2022-10-03 15:10:16 +0500

    a

--- Promises/Promises.js
+++ Promises/Promises.js
@@ -1,1 +1,45 @@
+console.clear()
 
+
+let array = [
+    {
+        title: "KeyBoard",
+        description: "TypeWriter Keyboard",
+    
+    }
+
+    ,{
+        title: "Keyboard",
+        description: "Simple KeyBoard"
+    }
+]
+
+function func(){
+ return new Promise(function(resolve, reject){
+    setTimeout(() => {
+        array.push({
+            title: "Keyboard1",
+            description: "Simple KeyBoard1"
+        })
+        
+
+        let error = true;
+        if( error == true){
+        resolve(
+            
+        )}
+        else{
+        reject()}
+        
+    }, 3000);})
+    
+}
+function func2(){
+    console.log(array)
+}
+
+
+
+func().then(function(){
+    func2()
+}).catch(function(){console.log("a")});
