65b719a12ff7b04a63ea3d4215699dbdfc878e94
Zyam-1 <zyammaqsood7@gmail.com>
2022-10-04 16:51:11 +0500

    ff

---
+++ CalcPackageNode/Calc.js
@@ -0,0 +1,9 @@
+console.clear();
+const calcfuncs = require ("./Calcfunc/exportFile.js");
+
+
+const sum = calcfuncs.addition;
+const prod = calcfuncs.multiplication;
+
+console.log(sum(3,6))
+console.log(prod(9,8))
---
+++ node_modules/zyam-calc/exportFile.js
@@ -0,0 +1,6 @@
+const sum = require("./sum");
+const prod = require("./product");
+
+const calcfuncs = {addition: sum, multiplication: prod};
+
+module.exports = calcfuncs;
