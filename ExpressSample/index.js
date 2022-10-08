5ac50e201fc2954de6fe879513dd2cb236eb9d07
Zyam-1 <zyammaqsood7@gmail.com>
2022-10-08 22:00:26 +0500

    Update index.js

--- ExpressSample/index.js
+++ ExpressSample/index.js
@@ -1,1 +1,13 @@
-const app = require("express");
+const express = require("express");
+const app = express();
+let array = ["Laptop", "Computer"];
+
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
+
+app.listen(3000);
+
+app.get("/api/Recipes", (req, res) => {
+  res.send(array);
+});
