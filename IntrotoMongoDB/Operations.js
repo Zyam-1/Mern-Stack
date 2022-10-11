e0c4d41c943fdb9f9fc4f2c1ef378351c1d71f37
Zyam <zyammaqsood7@gmail.com>
2022-10-11 14:33:20 +0500

    s

--- IntrotoMongoDB/Operations.js
+++ IntrotoMongoDB/Operations.js
@@ -8,4 +8,28 @@ const createProduct = async (name, price, tags) => {
    await Product.save();
    console.log(Product);
  };
- module.exports = createProduct;
+
+ const deleteProd = async (_id)=>{
+  let product = await ProductModel.findByIdAndDelete(_id);
+  return product;
+ }
+
+ const readProd = async ()=>{
+  let products = await ProductModel.find();
+  console.log(products);
+ }
+
+
+ const updatePord = async (_id, name, price, tags)=>{
+  let product = await ProductModel.findById(_id);
+  product.name = name;
+  product.price = price;
+  product.tags = tags;
+  await product.save()
+  return product;
+ }
+
+ module.exports.createProduct = createProduct;
+ module.exports.delProd = deleteProd;
+ module.exports.readProd = readProd;
+ module.exports.updatePord = updatePord;
--- IntrotoMongoDB/index.js
+++ IntrotoMongoDB/index.js
@@ -1,19 +1,22 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const createProduct = require("./Operations");
+const {createProduct, delProd, readProd, updatePord} = require("./Operations");
 app.use(express.json());
 
 
 mongoose
-  .connect("mongodb://localhost/MernStackn", {
+  .connect("mongodb://localhost/MernStack", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     family:4
   })
   .then(async() => {
     console.log("Connection to MongoDB created");
-    await createProduct ('Hp', 200,['laptop', 'green'] );
+    // await createProduct ('Dell', 299,['Laptop', 'Black'] );
+    // await delProd("634535cc7c87faf76d8ea2c8");
+    // await readProd();
+    await updatePord("634535a0f07b7333f8addbab", "HP updated", 299, ["Laptop, 6th Generation"])
     
   })
 
