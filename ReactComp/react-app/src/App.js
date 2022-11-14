913527ba3e2a4e83674e6b2bdc94d35899b9fe29
Zyam <zyammaqsood7@gmail.com>
2022-11-14 19:35:30 +0500

    First-React-App

---
+++ ReactComp/react-app/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Btn from "./Components/Btn";
+
+function App() {
+  return (
+    <div>
+        <Btn/>
+    </div>  );
+}
+
+export default App;
